8b900ef8e76bfca746cc3b8e5717970054a890ae
Michael Douglas <63735567+mdrgoncalves@users.noreply.github.com>
2022-07-19 17:00:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const pokemonNumber = document.querySelector('.pokemon-number');
+const pokemonName = document.querySelector('.pokemon-name');
+const pokemonImg = document.querySelector('.pokemon-img');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-search');
+
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+//Função de busca de pokemon
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }  
+}
+
+//Função de entrega de dados
+const renderPokemon = async (pokemon) => {
+
+    pokemonNumber.innerHTML = '';
+    pokemonName.innerHTML = 'Loading...';
+
+    const data = await fetchPokemon(pokemon);
+
+    if (data) {
+        pokemonNumber.innerHTML = data.id;
+        pokemonName.innerHTML = data.name;
+        pokemonImg.src = data.sprites.versions["generation-v"]["black-white"].animated["front_default"];
+        input.value = '';
+        searchPokemon = data.id;
+    } else {
+        pokemonNumber.innerHTML = '0';
+        pokemonName.innerHTML = 'Not Found';
+        pokemonImg.style.display = 'none';
+    }
+}
+
+//Função de retorno do formulário
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {;
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {;
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
